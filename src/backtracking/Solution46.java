12f286a2d0f498b70d356d3cfa9db5da72c6df63
vaderwang <2953381201@qq.com>
2018-12-02 10:19:55 +0800

    46. Permutations

--- src/backtracking/Solution46.java
+++ src/backtracking/Solution46.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Solution46 {
+
     private ArrayList<List<Integer>> results;
     private boolean[] used;
 
@@ -17,10 +18,10 @@ public class Solution46 {
         for (int i = 0; i < numbers.length; i ++) {
             if (!used[i]) {
                 permutation.addLast(numbers[i]);
-                used[index] = true;
+                used[i] = true;
                 generatePermutation(numbers, index+1, permutation);
                 permutation.removeLast();
-                used[index] = false;
+                used[i] = false;
             }
         }
     }
@@ -30,7 +31,7 @@ public class Solution46 {
         used = new boolean[numbers.length];
         LinkedList<Integer> permutation = new LinkedList<Integer>();
         generatePermutation(numbers, 0, permutation);
-        return null;
+        return results;
     }
 
     public static void main(String[] args) {
