e9199f990da716bf5edc3645ad66e37ed94e5d0e
vaderwang <2953381201@qq.com>
2018-12-05 19:38:19 +0800

    12. Integer to Roman

---
+++ src/explore/Solution14.java
@@ -0,0 +1,19 @@
+package explore;
+
+
+class Solution14 {
+
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        String[] dataOne = {"flower", "flow", "flight"};
+
+        String[] dataTwo = {"dog", "racecar", "car"};
+
+        String result = new Solution14().longestCommonPrefix(dataOne);
+        System.out.println(result);
+    }
+}
