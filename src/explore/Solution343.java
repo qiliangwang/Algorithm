9678fefd102f2557edb42fafc0f66977f99495cb
Vader Wang <2953381201@qq.com>
2018-12-06 21:03:35 +0800

    343. Integer Break

---
+++ src/explore/Solution343.java
@@ -0,0 +1,30 @@
+package explore;
+
+class Solution343 {
+    private int[] memo;
+
+    private int calcMax(int a, int b, int c) {
+        return Math.max(Math.max(a, b), c);
+    }
+    private int calcIntegerBreak(int n) {
+        memo[1] = 1;
+        for (int i = 2; i <= n; i ++) {
+            for (int j = 1; j <= i-1; j ++) {
+                //j + (i - j)
+                memo[i] = calcMax(j * memo[i - j], j * (i - j), memo[i]);
+            }
+        }
+        return memo[n];
+    }
+
+    public int integerBreak(int n) {
+        memo = new int[n + 1];
+        return calcIntegerBreak(n);
+    }
+
+    public static void main(String[] args) {
+
+        int result = new Solution343().integerBreak(10);
+        System.out.println(result);
+    }
+}
