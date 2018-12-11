8ad9c908e706cdc3273d7623f4ab37b45f16fdde
Vader Wang <2953381201@qq.com>
2018-12-11 22:23:36 +0800

    8. String to Integer (atoi)

--- src/leetcode/Solution8.java
+++ src/leetcode/Solution8.java
@@ -3,11 +3,31 @@ package leetcode;
 public class Solution8 {
 
     public int myAtoi(String str) {
-        return 0;
+        if (str == null) return 0;
+        str = str.trim();
+        if (str.length() == 0) return 0;
+        int sign = 1;
+        int start = 0;
+        long res = 0;
+        if (str.charAt(0) == '-') {
+            sign = -1;
+            start ++;
+        }else if (str.charAt(0) == '+') {
+            start ++;
+        }
+        for (int i = start; i < str.length(); i ++) {
+            if (! Character.isDigit(str.charAt(i))) {
+                return (int) res * sign;
+            }
+            res = res * 10 + str.charAt(i) - '0';
+            if (sign == -1 && res > Integer.MAX_VALUE) return Integer.MIN_VALUE;
+            if (sign == 1 && res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        }
+        return (int) res * sign;
     }
 
     public static void main(String[] args) {
-        int result = new Solution8().myAtoi("19087");
+        int result = new Solution8().myAtoi("9223372036854775808");
         System.out.println(result);
     }
 }
