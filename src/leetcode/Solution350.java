e2bd225bcd80c42c1f23be850a61c3b5ff6342ba
vaderwang <2953381201@qq.com>
2018-12-13 14:56:04 +0800

    350. Intersection of Two Arrays II

--- src/leetcode/Solution350.java
+++ src/leetcode/Solution350.java
@@ -1,23 +1,43 @@
 package leetcode;
 
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 class Solution350 {
 
     public int[] intersect(int[] nums1, int[] nums2) {
 
+        List<Integer> res = new ArrayList<>();
+
+        HashMap<Integer, Integer> numFreqHashMap = new HashMap<>();
+        for (int num: nums1) {
+            numFreqHashMap.put(num, numFreqHashMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num: nums2) {
+            if (numFreqHashMap.containsKey(num) && numFreqHashMap.get(num) > 0) {
+                res.add(num);
+                numFreqHashMap.put(num, numFreqHashMap.get(num) - 1);
+            }
+        }
+        int[] arr = new int[res.size()];
+        int idx = 0;
+        for (int ele : res) {
+            arr[idx++] = ele;
+        }
+        return arr;
+//        return res.stream().mapToInt(Integer::intValue).toArray();
     }
 
 
     public static void main(String[] args) {
-        String[] dataOne = {"flower", "flow", "flight"};
+        int[] nums1 = {1, 2, 2, 1};
 
-        String[] dataTwo = {"dog", "racecar", "car"};
+        int[] nums2 = {2, 2};
 
-        String result = new Solution350().intersect();
-        System.out.println(result);
+        int[] intersect = new Solution350().intersect(nums1, nums2);
+        for (int i : intersect) {
+            System.out.print(i + " ");
+        }
     }
 }
