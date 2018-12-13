e8eaf47d02949600fc05865777165f7deb5bd228
Vader Wang <2953381201@qq.com>
2018-12-13 10:33:01 +0800

    TODO

---
+++ src/leetcode/Solution14.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+
+class Solution14 {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        return "";
+
+    }
+
+
+    public static void main(String[] args) {
+        String[] dataOne = {"flower", "flow", "flight"};
+
+        String[] dataTwo = {"dog", "racecar", "car"};
+
+        String result = new Solution14().longestCommonPrefix(dataOne);
+        System.out.println(result);
+    }
+}
---
+++ src/leetcode/Solution350.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+class Solution350 {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+    }
+
+
+    public static void main(String[] args) {
+        String[] dataOne = {"flower", "flow", "flight"};
+
+        String[] dataTwo = {"dog", "racecar", "car"};
+
+        String result = new Solution350().intersect();
+        System.out.println(result);
+    }
+}
