6e4767735a58b5164e8e2ca5ab8a910057e0c5a9
vaderwang <2953381201@qq.com>
2018-12-14 16:15:32 +0800

    122. Best Time to Buy and Sell Stock II

--- src/leetcode/Solution122.java
+++ src/leetcode/Solution122.java
@@ -1,24 +1,24 @@
 package leetcode;
 
-class Solution121 {
+class Solution122 {
 
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length < 2)
             return 0;
-
-        int min = prices[0];
         int profit = 0;
 
-        for (int price: prices) {
-            min = Math.min(min, price);
-            profit = Math.max(price - min, profit);
+        for (int i = 1; i < prices.length; i ++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
         }
+
         return profit;
     }
 
     public static void main(String[] args) {
-        int[] prices = {7, 6, 4, 3, 1};
-        Solution121 numArray = new Solution121();
+        int[] prices = {1,2,3,4,5};
+        Solution122 numArray = new Solution122();
         int result = numArray.maxProfit(prices);
         System.out.println(result);
     }
