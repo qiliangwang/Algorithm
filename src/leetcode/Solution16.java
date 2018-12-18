088f831003a0c7fa1a5a2dbce6913184dedf39ac
Vader Wang <2953381201@qq.com>
2018-12-18 10:00:12 +0800

    16. 3Sum Closest

---
+++ src/leetcode/Solution16.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution16 {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int results = nums[0] + nums[1] + nums[nums.length - 1];
+        for (int i = 0; i < nums.length - 2; i ++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > target) {
+                    right --;
+                }else {
+                 left ++;
+                }
+                if (Math.abs(sum - target) < Math.abs(results - target)) {
+                    results = sum;
+                }
+            }
+        }
+        return results;
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = {-1, 2, 1, -4};
+        int[] nums = {0, 0, 0};
+        int result = new Solution16().threeSumClosest(nums, -1);
+        System.out.println(result);
+    }
+}
