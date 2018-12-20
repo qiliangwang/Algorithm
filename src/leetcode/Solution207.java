82db4a5c72f2d63842cd281b1376a2b9aa2926d5
vaderwang <2953381201@qq.com>
2018-12-20 21:04:32 +0800

    207. Course Schedule

--- src/leetcode/Solution207.java
+++ src/leetcode/Solution207.java
@@ -1,12 +1,36 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 class Solution207 {
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        return false;
+        int[] indegree = new int[numCourses];
+        int result = numCourses;
+        //cal into the degree
+        for (int[] pair : prerequisites) {
+            indegree[pair[0]]++;
+        }
+        //find node where into the degree is zero
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0)
+                queue.offer(i);
+        }
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            result--;
+            for (int[] pair : prerequisites) {
+                if (pair[1] == pre) {
+                    indegree[pair[0]]--;
+                    if (indegree[pair[0]] == 0)
+                        queue.offer(pair[0]);
+                }
+            }
+        }
+        return result == 0;
     }
 
 
