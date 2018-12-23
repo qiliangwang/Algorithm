f4b2e1253b51e44a8ffd8574780cff3af6cec7db
Vader Wang <2953381201@qq.com>
2018-12-23 20:17:36 +0800

    14. Longest Common Prefix

--- src/leetcode/Solution14.java
+++ src/leetcode/Solution14.java
@@ -4,9 +4,14 @@ package leetcode;
 class Solution14 {
 
     public String longestCommonPrefix(String[] strs) {
-
-        return "";
-
+        if (strs == null || strs.length == 0) return "";
+        String res = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(res) != 0) {
+                res = res.substring(0, res.length() - 1);
+            }
+        }
+        return res;
     }
 
 
@@ -15,7 +20,7 @@ class Solution14 {
 
         String[] dataTwo = {"dog", "racecar", "car"};
 
-        String result = new Solution14().longestCommonPrefix(dataOne);
+        String result = new Solution14().longestCommonPrefix(dataTwo);
         System.out.println(result);
     }
 }
