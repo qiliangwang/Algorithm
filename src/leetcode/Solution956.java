59e55f210f10723be5e7bf5f9d4db5d76d1c8ffe
Vader Wang <2953381201@qq.com>
2018-12-23 22:47:18 +0800

    22. Generate Parentheses

---
+++ src/leetcode/Solution956.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Solution956 {
+
+    public int tallestBillboard(int[] rods) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] roads = {1, 2, 3, 6};
+        int result = new Solution956().tallestBillboard(roads);
+        System.out.println(result);
+    }
+}
