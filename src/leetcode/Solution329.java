b534bd579c933eb65daedd6ba974beb0122451e4
vaderwang <2953381201@qq.com>
2018-12-28 09:18:29 +0800

    329. Longest Increasing Path in a Matrix

--- src/leetcode/Solution329.java
+++ src/leetcode/Solution329.java
@@ -8,7 +8,6 @@ public class Solution329 {
      * From each cell, you can either move to four directions: left, right, up or down.
      * You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
      *
-     * Example 1:
      *
      * Input: nums =
      * [
@@ -18,7 +17,6 @@ public class Solution329 {
      * ]
      * Output: 4
      * Explanation: The longest increasing path is [1, 2, 6, 9].
-     * Example 2:
      *
      * Input: nums =
      * [
@@ -28,8 +26,6 @@ public class Solution329 {
      * ]
      * Output: 4
      * Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
-     * @param matrix
-     * @return
      */
 
     private int m, n;
@@ -38,10 +34,11 @@ public class Solution329 {
     private int[][] d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        int res = 0;
         m = matrix.length;
         n = matrix[0].length;
         cache = new int[m][n];
-        int res = 0;
         for (int i = 0; i < m; i ++) {
             for (int j = 0; j < n; j ++) {
                 int temp = dfs(matrix, i, j, Integer.MIN_VALUE);
