6c2c5228c8c43ee1515c4f66a155cd04151384dc
vaderwang <2953381201@qq.com>
2018-12-29 19:53:49 +0800

    TODO

---
+++ src/leetcode/Solution239.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution239 {
+
+    /**
+     * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+     *
+     * Example:
+     *
+     * Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+     * Output: [3,3,5,5,6,7]
+     * Explanation:
+     *
+     * Window position                Max
+     * ---------------               -----
+     * [1  3  -1] -3  5  3  6  7       3
+     *  1 [3  -1  -3] 5  3  6  7       3
+     *  1  3 [-1  -3  5] 3  6  7       5
+     *  1  3  -1 [-3  5  3] 6  7       5
+     *  1  3  -1  -3 [5  3  6] 7       6
+     *  1  3  -1  -3  5 [3  6  7]      7
+     * Note:
+     * You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+     *
+     * Follow up:
+     * Could you solve it in linear time?
+     *
+     * what deque remains is the index of max value
+     * @param nums [1,3,-1,-3,5,3,6,7]
+     * @param k 3
+     * @return [3,3,5,5,6,7]
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return new int[0];
+
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length + 1 - k];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() == i - k) {
+                deque.poll();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.removeLast();
+            }
+            deque.offerLast(i);
+            if ((i + 1) >= k) {
+                res[i + 1 - k] = nums[deque.peek()];
+            }
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int[] result = new Solution239().maxSlidingWindow(nums, 3);
+        for (int res : result) {
+            System.out.print(res + " ");
+        }
+    }
+}
---
+++ src/leetcode/Solution639.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Solution639 {
+
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        boolean result = new Solution639().isMatch("19087", "a");
+        System.out.println(result);
+    }
+}
