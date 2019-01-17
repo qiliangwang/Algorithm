e28aab11cc5459e2771bdce87b7bbb61402769ba
vaderwang <2953381201@qq.com>
2019-01-17 19:36:14 +0800

    TODO

---
+++ src/leetcode/RandomListNode.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * @author Vader Wang
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next, random;
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
---
+++ src/leetcode/Solution138.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * @author Vader Wang
+ */
+class Solution138 {
+
+    public RandomListNode copyRandomList(RandomListNode head) {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        RandomListNode result = new Solution138().copyRandomList(null);
+    }
+}
