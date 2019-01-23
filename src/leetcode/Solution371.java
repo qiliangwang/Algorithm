10af88d8ca1c5d7502db66c1e8abe7b36d9a19b3
Vader Wang <2953381201@qq.com>
2019-01-23 23:13:16 +0800

    371. Sum of Two Integers

---
+++ src/leetcode/Solution371.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class Solution371 {
+
+    /**
+     * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+     Example:
+     Given a = 1 and b = 2, return 3.
+
+     time : O(1) / O(n)
+     space : O(1)
+
+     * @param a
+     * @param b
+     * @return
+     */
+    public int getSum(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int result = new Solution371().getSum(1, 2);
+        System.out.println(result);
+    }
+}
