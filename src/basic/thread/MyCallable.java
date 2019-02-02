dc70cfabc2842aaaaab81c5bcf193001be0e7115
Vader Wang <2953381201@qq.com>
2019-02-02 11:41:06 +0800

    callable demo

--- src/basic/thread/MyCallable.java
+++ src/basic/thread/MyCallable.java
@@ -5,10 +5,10 @@ import java.util.concurrent.Callable;
 public class MyCallable implements Callable<String> {
     @Override
     public String call() throws Exception{
-        String value="test";
+        String value="Callable return value";
         System.out.println("Ready to work");
         Thread.currentThread().sleep(5000);
-        System.out.println("task done");
+        System.out.println("finish work");
         return  value;
     }
 
