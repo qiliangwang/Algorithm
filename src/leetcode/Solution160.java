0f9c6b7888f198ea54679baacc04113a32ff6d49
Vader Wang <2953381201@qq.com>
2019-02-12 19:19:02 +0800

    Intersection of Two Linked Lists

---
+++ src/leetcode/Solution160.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * @author Vader Wang
+ */
+public class Solution160 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a.val != b.val) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA :b.next;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
