f062212241875834763b01e17acdd5cccca2f007
Vader Wang <2953381201@qq.com>
2019-02-13 22:57:11 +0800

    10. Regular Expression Matching

--- src/leetcode/Solution239.java
+++ src/leetcode/Solution239.java
@@ -6,7 +6,8 @@ import java.util.LinkedList;
 public class Solution239 {
 
     /**
-     * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+     * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
+     * You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
      *
      * Example:
      *
