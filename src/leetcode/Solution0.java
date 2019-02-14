9071729d904ccc399293d70476bbdb6f22c2f350
Vader Wang <2953381201@qq.com>
2019-02-14 18:44:20 +0800

    387

---
+++ src/leetcode/Solution0.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Solution0 {
+
+    public String convert(String s, int numRows) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String result = new Solution0().convert("PAYPALISHIRING", 4);
+        System.out.println(result);
+    }
+}
