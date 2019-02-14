e5484cbbcf76588adfdb34b88adda56fb0e56343
Vader Wang <2953381201@qq.com>
2019-02-14 21:03:50 +0800

    35. Search Insert Position

---
+++ src/leetcode/Solution0.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Solution0 {
+
+    public String longestPalindrome(String s) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String result = new Solution0().longestPalindrome("c");
+        System.out.println(result);
+    }
+}
