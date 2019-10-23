76cf9a4e8152564c131e867e22885548efedcae6
qiliangwang <2953381201@qq.com>
2019-10-23 14:27:27 +0800

    medianFinder

--- src/leetcode/solutions/Solution295.java
+++ src/leetcode/solutions/Solution295.java
@@ -7,6 +7,11 @@ public class Solution295 {
     public static void main(String[] args) {
         MedianFinder medianFinder = new MedianFinder();
         medianFinder.addNum(1);
+        medianFinder.addNum(2);
+        medianFinder.addNum(3);
+        medianFinder.addNum(0);
+        medianFinder.addNum(4);
+        medianFinder.addNum(1);
         double result = medianFinder.findMedian();
         System.out.println(result);
     }
