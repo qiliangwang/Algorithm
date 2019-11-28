3e7614cead196c293cc64ddbe4ee569c06bb91ce
qiliangwang <2953381201@qq.com>
2019-11-28 15:33:46 +0800

    cut.point && cut.bridge

---
+++ src/graph/cut/bridge/Edge.java
@@ -0,0 +1,16 @@
+package graph.cut.bridge;
+
+public class Edge {
+
+    private int v, w;
+
+    public Edge(int v, int w){
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%d-%d", v, w);
+    }
+}
